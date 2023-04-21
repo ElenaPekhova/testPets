c9313b3bfecb34e539d55fed9465d3f2f960d772
ElenaPekhova <124542306+ElenaPekhova@users.noreply.github.com>
2023-04-21 17:19:36 +0800

    Add files via upload

---
+++ Tests_Petsfriends/settings.py
@@ -0,0 +1,6 @@
+valid_email = "testerQAP@yandex.ru"
+valid_password = "12345"
+empty_email = ""
+empty_password = ""
+empty_auth_key = {"key": ""}
+incorrect_auth_key = {"key": "000000000000000000000000000000000000000000000"}
